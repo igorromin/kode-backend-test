81d249bdc0900f71146523a5d30450d710d62602
igorromin <igorromin@yandex.ru>
2019-07-18 12:37:12 +0700

    Change dev to prod

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=igorrodi_api',
+    'username' => 'igorrodi_api',
+    'password' => '3t2%V6WN',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
